9c9e2351b272665e9d3643a37a4dbaca21ce4f8f
dahyun <fbekgus413@naver.com>
2024-09-17 00:10:09 +0900

    #1934

--- baekjonn_c++/1934.c
+++ baekjonn_c++/1934.c
@@ -1,10 +1,29 @@
 #include<stdio.h>
-int main(){
-    int T=0, a[1000]={0,}, b[1000]={0,};
-    scanf("%d", &T);
-    if (int i=0; i<T; i++){
-        scanf("%d %d", &a[i], &b[i]);
-    }
+/*최대 공약수*/
+int Max(int a, int b){
+    int result1 = 0;
+    int min = (a<b)?a:b;
 
-    
+    for (int c = 1; c<= min; c++){
+        if((a%c == 0)&&(b%c==0)){
+            result1 = c;
+            if(result1 < c){
+                result1 = c;
+            }
+        }
+    }
+    return result1;
+}
+/*최소 공배수*/
+int Min(int a, int b){
+    return a*b/Max(a,b);
+}
+int main(void){
+    int num, a, b;
+    scanf("%d", &num);
+
+    for (int i = 0; i<num; i++){
+        scanf("%d %d", &a, &b);
+        printf("%d\n", Min(a,b));
+    }
 }
