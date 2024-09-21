08396720876883e68f1e7b39c945004b0f4f332f
dahyun <fbekgus413@naver.com>
2024-09-21 23:52:42 +0900

    #10872

---
+++ baekjonn_python/10872.py
@@ -0,0 +1,17 @@
+# import sys
+# N = int(sys.stdin.readline())
+# k = 1
+# if N>0:
+#     for i in range(1,N+1):
+#         k *= i
+# print(k)
+
+#함수로 만들어 보기
+def factory (n):
+    result = 1
+    if n>0:
+        result *= 
+    return n
+
+n = int(input())
+print(factory(n))
