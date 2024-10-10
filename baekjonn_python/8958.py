987b493e2fc95b3908df65afbce169bb178b036c
dahyun <fbekgus413@naver.com>
2024-10-10 13:27:23 +0900

    #8958 **

--- baekjonn_python/8958.py
+++ baekjonn_python/8958.py
@@ -1,1 +1,13 @@
+num = int(input())
 
+for i in range(num):
+    test = list(input())
+    numbers = 0
+    results=0
+    for j in test:
+        if j == 'O':
+            numbers += 1
+            results += numbers
+        else:
+            numbers = 0
+    print(results)
